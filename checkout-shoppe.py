3f07f20ca3f2b32bde45e2c9aaa5f373beee91fe
MangArya11 <komanggede86@gmail.com>
2020-12-13 12:40:23 +0800

    commit

--- checkout-shoppe.py
+++ checkout-shoppe.py
@@ -3,66 +3,64 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 login_qr = "https://shopee.co.id/buyer/login/qr?next=https%3A%2F%2Fshopee.co.id%2F"
-product_name = "Apple-iPhone-11-Pro-512GB-Midnight-Green-i.255563049.6435648695"
+product_name = "POLICE-Kemeja-Regular-Fit-Pria-621060774-Biru-i.56529142.6052840877"
 flash_sale = login_qr + product_name
 
 class PrepareBot:
-    def prepare(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get(flash_sale)
-        checkOutBot = CheckOutBot()
-
     def countdownTimer(self):
-        target_m = 59
+        target_m =59
         target_s = 60
         current_m = time.strftime("%M")
         current_s = time.strftime("%S")
         minutes = target_m - int(current_m)
         seconds = target_s - int(current_s)
-        total_second = minutes * 60 + seconds - 1
+        # total_second = minutes * 60 + seconds - 1
+        total_second = 00 * 60 + 61 - 1
         while total_second:
             mins, secs = divmod(total_second, 60)
             print(f'{mins:02d}:{secs:02d}', end='\r')
             time.sleep(1)
             total_second -= 1
-        checkOutBot.addProduct()
-
-    
 
 
 class CheckOutBot(PrepareBot):
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(flash_sale)
+
     def addProduct(self):
         # selectColor = self.driver.find_element_by_xpath(
         #     '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div[5]/div/div[1]/div/button'
         #     ).click()
-        # selectModel = self.driver.find_element_by_xpath(
-        #     '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div[3]/div/div[1]/div/button[4]'
-        #     ).click()
+        selectModel = self.driver.find_element_by_xpath(
+            '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div[4]/div/div[1]/div/button[1]'
+            ).click()
         add_to_cart = self.driver.find_element_by_xpath(
             '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[5]/div/div/button[2]'
             ).click()
     
     def getProduct(self):
+        time.sleep(5)
         checkout = self.driver.find_element_by_xpath(
-            '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[5]/div/div/button'
+            '//*[@id="main"]/div/div[2]/div[2]/div[3]/div[2]/div[7]/div[5]/button'
             ).click()
-        time.sleep(6)
-        make_order = self.driver.find_elements_by_class_name(
-            '.stardust-button stardust-button--primary stardust-button--large _22Ktrb'
+        time.sleep(5)
+        make_order = self.driver.find_element_by_xpath(
+            '//*[@id="main"]/div/div[2]/div[3]/div[4]/div[2]/div[12]/button'
             ).click()   
 
 
-
-
 if __name__ == "__main__":
+    checkOutBot = CheckOutBot()
     prepareBot = PrepareBot()
-    prepareBot.prepare()
-    prepareBot.countdownTimer()
 
-    time.sleep(5)
-    
+    prepareBot.countdownTimer()
+    checkOutBot.addProduct()
+
     checkOutBot.getProduct()
     time.sleep(60)
     
 
-    
+
+
+# //*[@id="main"]/div/div[2]/div[3]/div[4]/div[2]/div[9]/button
